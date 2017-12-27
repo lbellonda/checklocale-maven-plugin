f06cbae822bfee18f3c699cc692ace5aa46d6e2e
Luca Bellonda <lbellonda@gmail.com>
2017-12-28 00:10:12 +0100

    Optional parameter to turn errors into warnings.

--- src/main/java/checklocale/mvnplugin/CPMojo.java
+++ src/main/java/checklocale/mvnplugin/CPMojo.java
@@ -32,83 +32,97 @@ import java.util.List;
 
 /**
  * This goal checks the localizations files.
+ * 
  * @goal checklocale
  * 
  * @phase compile
  */
-public class CPMojo
-    extends AbstractMojo
-{
+public class CPMojo extends AbstractMojo {
 	private static final String MOJO_NAME = "CPMojo";
-    /**
-     * Encoding of the properties
-     * @parameter encoding, defaultValue = "UTF-8"
-     */
-    private String encoding;
-	
-    /**
-     * Folders to check
-     * @parameter directories
-     * @required
-     */
+
+	/**
+	 * Flag to fire errors, if false only warnings will be issued
+	 * 
+	 * @parameter errors, defaultValue = true
+	 */
+	private boolean errors = true ;
+
+	/**
+	 * Encoding of the properties
+	 * 
+	 * @parameter encoding, defaultValue = "UTF-8"
+	 */
+	private String encoding;
+
+	/**
+	 * Folders to check
+	 * 
+	 * @parameter directories
+	 * @required
+	 */
 	private String[] directories;
-	
-    /**
-     * Project folder
-    * @parameter default-value="${basedir}"
-    * @readonly
-    */
+
+	/**
+	 * Project folder
+	 * 
+	 * @parameter default-value="${basedir}"
+	 * @readonly
+	 */
 	private String projectBaseDir;
-	
-    public void execute()
-        throws MojoExecutionException, MojoFailureException
-    {
-    	try {
-	    	getLog().debug(MOJO_NAME +" started, checking properties");
-	    	getLog().debug(MOJO_NAME +" context: "+getPluginContext().toString());
-	    	
-	    	getLog().debug(MOJO_NAME +" parameters:");
-	    	getLog().debug("   encoding:"+encoding);
-	    	getLog().debug("   baseDir:"+projectBaseDir);
-	    	List<String> folders = getListFromFolders();
-	    	if( (null == directories) || (directories.length==0)) {
-	    		getLog().debug(MOJO_NAME +" no folders to check.");
-	    		return ;
-	    	}
-	    	Configuration configuration = new Configuration();
-	    	if( (null != encoding ) && ( encoding.length()>0 ) ) {
-	    		configuration.setEncoding(encoding);
-	    	}
-	    	configuration.setDirectories(folders);
-	    	configuration.setBaseDir(projectBaseDir);
-	    	getLog().debug(MOJO_NAME +" starting execution");
-	    	Execution execution = new Execution();
-	    	List<PError> errors = execution.execute(configuration);
-	    	if(errors.isEmpty()) {
-	    		getLog().info(MOJO_NAME +" end execution, no errors.");
-	    	} else {
-	    		getLog().error(MOJO_NAME +" found "+errors.size()+" error(s).");
-	    		for(PError error : errors ) {
-	    			getLog().error(error.toString());
-	    		}
-	    		throw new MojoFailureException(MOJO_NAME +" found "+errors.size()+" error(s).");
-	    	}
-    	} catch ( Exception ex )
-        {
-            throw new MojoExecutionException( "Error executing "+MOJO_NAME, ex );
-        }
-    }
-    
-    private List<String> getListFromFolders()
-    {
-    	List<String> folders = new ArrayList<String>();
-    	if(null != directories) {
-    		for( String aFolder : directories ) {
-    			folders.add(aFolder);
-    			getLog().debug(MOJO_NAME+ " add directory:"+aFolder);
-    		}
-    	}
-    	getLog().debug(MOJO_NAME+ " directories count:"+folders.size());
-    	return folders;
-    }
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		try {
+			getLog().debug(MOJO_NAME + " started, checking properties");
+			getLog().debug(MOJO_NAME + " context: " + getPluginContext().toString());
+
+			getLog().debug(MOJO_NAME + " parameters:");
+			getLog().debug("   errors:" + errors);
+			getLog().debug("   encoding:" + encoding);
+			getLog().debug("   baseDir:" + projectBaseDir);
+			List<String> folders = getListFromFolders();
+			if ((null == directories) || (directories.length == 0)) {
+				getLog().debug(MOJO_NAME + " no folders to check.");
+				return;
+			}
+			Configuration configuration = new Configuration();
+			if ((null != encoding) && (encoding.length() > 0)) {
+				configuration.setEncoding(encoding);
+			}
+			configuration.setDirectories(folders);
+			configuration.setBaseDir(projectBaseDir);
+			getLog().debug(MOJO_NAME + " starting execution");
+			Execution execution = new Execution();
+			List<PError> errorsList = execution.execute(configuration);
+			if (errorsList.isEmpty()) {
+				getLog().info(MOJO_NAME + " end execution, no errors.");
+			} else {
+				if (errors) {
+					getLog().error(MOJO_NAME + " found " + errorsList.size() + " error(s).");
+					for (PError error : errorsList) {
+						getLog().error(error.toString());
+					}
+					throw new MojoFailureException(MOJO_NAME + " found " + errorsList.size() + " error(s).");
+				} else {
+					getLog().warn(MOJO_NAME + " found " + errorsList.size() + " error(s).");
+					for (PError error : errorsList) {
+						getLog().warn(error.toString());
+					}
+				}
+			}
+		} catch (Exception ex) {
+			throw new MojoExecutionException("Error executing " + MOJO_NAME, ex);
+		}
+	}
+
+	private List<String> getListFromFolders() {
+		List<String> folders = new ArrayList<String>();
+		if (null != directories) {
+			for (String aFolder : directories) {
+				folders.add(aFolder);
+				getLog().debug(MOJO_NAME + " add directory:" + aFolder);
+			}
+		}
+		getLog().debug(MOJO_NAME + " directories count:" + folders.size());
+		return folders;
+	}
 }
